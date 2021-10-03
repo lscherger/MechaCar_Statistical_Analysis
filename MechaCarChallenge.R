aa3e04ca0408776bd01784f74c0af13bdd637360
Leslie Scherger <lesliescherger@Leslies-MacBook-Air.local>
2021-10-03 14:01:48 -0500

    adding linear regression to script file.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,2 @@
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar))
